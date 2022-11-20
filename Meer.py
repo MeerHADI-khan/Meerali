4ff7f6ff131d5f23a09a8a67cdb359a3b9414891
MR-HADI <118607848+MeerHADI-khan@users.noreply.github.com>
2022-11-20 07:59:51 +0500

    Meer.py
    pkg update
     pkg upgrade
     pkg install python
    pkg install python2
     pip install requests
     pip install mechanize
     pip install lolcat
     pip install rich
     pip install BeautifulSoup
     pkg install git
    Git clonehttps://github.com/MeerHADI-khan/Meerali.git
    ls
    cd Meerali
    ls
    python Meer.py

---
+++ Meer.py
@@ -0,0 +1,101 @@
+<p align="center"><p 
+| "center">
+<a href="https://MEERHADI-KHAN.github.io/"><img title="Made in BANGLADESH" src="https://img.shields.io/badge/MADE%20IN-BANGLADESH-SCRIPT?colorA=%23ff8100&colorB=%23017e40&colorC=%23ff0000&style=for-the-badge"></a>
+
+<a href="https://MEERHADI-KHAN.github.io/"><img title="Version" src="https://img.shields.io/badge/Version-1.0.5-green.svg?style=flat-square"></a>
+
+<h3 align="center">
+  <img src="https://emoji.discord.st/emojis/768b108d-274f-4f44-a634-8477b16efce7.gif" width="25">
+  &nbsp;   ASLAM    Alikum    &nbsp;
+  <img src="https://emoji.discord.st/emojis/768b108d-274f-4f44-a634-8477b16efce7.gif" width="25">
+
+![boysdpz_c4663e3_1656850427534_sc_cmprsd_75](https://user-images.githubusercontent.com/114737357/196397178-9b25dc90-22fb-4a00-8093-2905c06b034d.jpg)
+
+</h3>
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-1.svg">
+
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%23F72121&size=25&duration=7000&center=true&vCenter=true&multiline=true&height=100&lines=🌹+I+am+MEER+HADi+🌹)](https://git.io/typing-svg)
+
+![106824690-8dd73a00-66ad-11eb-89e2-53e13ac6f594 (1)](https://user-images.githubusercontent.com/79738922/150628863-e161ecb3-06fe-4656-be20-9122ed533309.gif)
+
+![Alt text](https://github.com/MRVIVEK-CODER/MRVIVEK-CODER/raw/main/md7Oqrf.gif)
+
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%23F70B10&size=27&lines=😍+Assalamu+Wallaikum+🖤;+👿+It's+Not+Just+A+Name+,;It's+A+Brand,,👿;Thank+You+Everyone+🥰)](https://git.io/typing-svg)
+
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=SANI-KHAN&label=Profile%20views&color=0e75b6&style=flat" alt="SANI-KHAN" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=MEERHADI-KHAN" alt="MEERHADI-khan" /></a> </p>
+
+<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+<img width="800px" src="https://user-images.githubusercontent.com/116461/76165260-c6c00500-6112-11ea-9cda-0a6cb9b72e8f.gif" />
+
+•Ⓑ• <b>VISTOR COUNT •Ⓑ•
+
+  <img src="https://profile-counter.glitch.me/N1ght420/count.svg" />
+
+- Contact Me **Meershahzeba@gmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://www.facebook.com/profile.php?id=100076381339868" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="Baizid.171" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://hive.apache.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" alt="hive" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://kotlinlang.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=alifvau&show_icons=true&locale=en&layout=compact" alt="alifvau" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=alifvau&show_icons=true&locale=en" alt="alifvau" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=alifvau&" alt="alifvau" /></p>
+
+<a href="https://MEERHADI-KHAN.github.io/"><img title="Made in Pakistan" src="https://img.shields.io/badge/MADE%20IN-PAKISTAN -SCRIPT?colorA=%23ff8100&colorB=%23017e40&colorC=%23ff0000&style=for-the-badge"></a>
+
+<a href="https://MEERHAI-KHAN.github.io/"><img title="Version" src="https://img.shields.io/badge/Version-0.1.8-green.svg?style=flat-square"></a>
+
+<h3 align="center">
+  <[![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&pause=800&color=D4202000&width=435&lines=MR+HADI%F0%9F%91%BF%F0%9F%91%BF%F0%9F%91%BF+naam+to+suna+hoga;Har+Kisi+Ke+bus+Ki+Baat+Nahin+k+MR+HADi+se+baat+kare%F0%9F%91%BF%F0%9F%98%92)](https://git.io/typing-svg)>
+
+![boysdpz_c4663e3_1656850427534_sc_cmprsd_75](https://user-images.githubusercontent.com/114737357/196397178-9b25dc90-22fb-4a00-8093-2905c06b034d.jpg)
+
+</h3>
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-1.svg">
+
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Fira+Code&pause=800&color=D4202000&width=435&lines=MR+HADI%F0%9F%91%BF%F0%9F%91%BF%F0%9F%91%BF+naam+to+suna+hoga;Har+Kisi+Ke+bus+Ki+Baat+Nahin+k+MR+HADi+se+baat+kare%F0%9F%91%BF%F0%9F%98%92" alt="Typing SVG" /></a>
+
+![106824690-8dd73a00-66ad-11eb-89e2-53e13ac6f594 (1)](https://user-images.githubusercontent.com/79738922/150628863-e161ecb3-06fe-4656-be20-9122ed533309.gif)
+
+![Alt text](https://github.com/MRVIVEK-CODER/MRVIVEK-CODER/raw/main/md7Oqrf.gif)
+
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%23F70B10&size=27&lines=😍+Assalamu+Wallaikum+🖤;+👿+It's+Not+Just+A+Name+,;It's+A+Brand,,👿;Thank+You+Everyone+🥰)](https://git.io/typing-svg)
+
+
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=MEERHADI-Khan&label=Profile%20views&color=0e75b6&style=flat" alt="SANI-KHAN" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=MEERHADI-KHAN" alt="MEERHADI-KHAN" /></a> </p>
+
+<p align="left"> <a href="https://twitter.com/" target="blank"><img src="https://img.shields.io/twitter/follow/?logo=twitter&style=for-the-badge" alt="" /></a> </p>
+
+<img width="800px" src="https://user-images.githubusercontent.com/116461/76165260-c6c00500-6112-11ea-9cda-0a6cb9b72e8f.gif" />
+
+•Ⓑ• <b>VISTOR COUNT •Ⓑ•
+
+  <img src="https://profile-counter.glitch.me/N1ght420/count.svg" />
+
+- Contact Me **Meershahzebz@gmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://www.facebook.com/sanikhan570" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="Baizid.171" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://hive.apache.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/apache_hive/apache_hive-icon.svg" alt="hive" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://kotlinlang.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=alifvau&show_icons=true&locale=en&layout=compact" alt="alifvau" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=alifvau&show_icons=true&locale=en" alt="alifvau" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=alifvau&" alt="alifvau" /></p>
